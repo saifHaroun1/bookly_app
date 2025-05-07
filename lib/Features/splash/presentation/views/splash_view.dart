2dfa85f68b754a0f0d6bf499d4c772886ae6c522
saifHaroun1 <saif_h06999@cic-cairo.com>
2025-05-07 12:39:22 +0300

    first commit

---
+++ lib/Features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:my_bookly_app/Features/splash/presentation/views/widgets/splash_view_body.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SplashViewBody());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:my_bookly_app/Features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(home: SplashView());
+  }
+}
