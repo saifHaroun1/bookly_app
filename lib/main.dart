6d3feb00f24fe96cffb6d687729bdd69ad5a7e59
bassilharoun <bassilharoun123@gmail.com>
2025-07-13 01:10:59 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:bookly_app/Features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(home: SplashView());
+  }
+}
