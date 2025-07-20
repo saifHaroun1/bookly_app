c625ac326f50da084b69e09dc3abe4c2a35e3b32
bassilharoun <bassilharoun123@gmail.com>
2025-07-20 09:14:01 +0300

    create api services

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,16 @@
+import 'package:bookly_app/Features/home/data/model/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:bookly_app/core/utils/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    throw UnimplementedError();
+  }
+}
