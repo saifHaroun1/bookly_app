7c650554f1ccfde8b39cbdd563a24aec518f86a3
bassilharoun <bassilharoun123@gmail.com>
2025-07-21 08:26:05 +0300

    newset books cubit done

--- lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/model/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewsedBooks();
+
+    result.fold(
+      (Failure) {
+        emit(NewsetBooksFailure(errMessage: Failure.errMessage));
+      },
+      (books) {
+        emit(NewsetBooksSuccess(books: books));
+      },
+    );
+  }
 }
